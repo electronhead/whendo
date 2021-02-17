03f558dad4d62f5518024917ad2ffbd97e7cab43
electronhead <gary.beaver@electronhead.com>
2021-02-17 15:58:01 -0800

    workaround for pi build issue

--- setup.py
+++ setup.py
@@ -6,3 +6,10 @@ setuptools.setup() looks for configuration information in setup.cfg.
 import setuptools
 
 setuptools.setup()
+
+"""
+Added as a workaround so that...
+    >> python setup.py sdist bdist_wheel
+...would work on raspberry pi os using a setup.cfg file.
+"""
+packages = setuptools.find_packages(include=['core', 'api', 'sdk'], exclude=['test'])
